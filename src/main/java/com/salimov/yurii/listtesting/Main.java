31fbcc082cc97a591cf358d81c974fe71f2dc134
YuriiSalimov <yuriy.alex.salimov@gmail.com>
2017-05-12 09:24:19 +0300

    Code refactoring.

--- src/main/java/com/salimov/yurii/listtesting/Main.java
+++ src/main/java/com/salimov/yurii/listtesting/Main.java
@@ -2,67 +2,51 @@ package com.salimov.yurii.listtesting;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Vector;
 
 /**
  * @author Yurii Salimov (yuriy.alex.salimov@gmail.com)
- * @version 1.0
  */
 public class Main {
 
     private final static int COUNT = 10;
 
     public static void main(String[] args) {
-        testArrayList();
-        testLinkedList();
-        testVector();
+        System.out.println();
+        testList(ArrayList.class);
+        testList(LinkedList.class);
+        testList(Vector.class);
     }
 
-    private static void testArrayList() {
+    private static void testList(Class<? extends List> clazz) {
         double timeTestFilling = 0;
         double timeTestGettingByIndex = 0;
         double timeTestContains = 0;
+        double timeTestRemove = 0;
         for (int i = 0; i < COUNT; i++) {
-            timeTestFilling += ListTester.testFilling(new ArrayList<>());
-            timeTestGettingByIndex += ListTester.testGettingByIndex(new ArrayList<>());
-            timeTestContains += ListTester.testContains(new ArrayList<>());
+            timeTestFilling += ListTester.testFilling(createList(clazz));
+            timeTestGettingByIndex += ListTester.testGettingByIndex(createList(clazz));
+            timeTestContains += ListTester.testContains(createList(clazz));
+            timeTestRemove += ListTester.testRemove(createList(clazz));
         }
-        System.out.println("ArrayList result:");
+        System.out.println(clazz.getSimpleName() + " result:");
         System.out.println("1) testFilling() = " + (timeTestFilling / COUNT) + " ms");
         System.out.println("2) testGettingByIndex() = " + (timeTestGettingByIndex / COUNT) + " ms");
         System.out.println("3) testContains() = " + (timeTestContains / COUNT) + " ms");
+        System.out.println("4) testRemove() = " + (timeTestRemove / COUNT) + " ms");
         System.out.println();
     }
 
-    private static void testLinkedList() {
-        double timeTestFilling = 0;
-        double timeTestGettingByIndex = 0;
-        double timeTestContains = 0;
-        for (int i = 0; i < COUNT; i++) {
-            timeTestFilling += ListTester.testFilling(new LinkedList<>());
-            timeTestGettingByIndex += ListTester.testGettingByIndex(new LinkedList<>());
-            timeTestContains += ListTester.testContains(new LinkedList<>());
+    private static List<Integer> createList(Class clazz) {
+        List<Integer> result;
+        if (clazz == ArrayList.class) {
+            result = new ArrayList<>();
+        } else if (clazz == LinkedList.class) {
+            result = new LinkedList<>();
+        } else {
+            result = new Vector<>();
         }
-        System.out.println("LinkedList result:");
-        System.out.println("1) testFilling() = " + (timeTestFilling / COUNT) + " ms");
-        System.out.println("2) testGettingByIndex() = " + (timeTestGettingByIndex / COUNT) + " ms");
-        System.out.println("3) testContains() = " + (timeTestContains / COUNT) + " ms");
-        System.out.println();
-    }
-
-    private static void testVector() {
-        double timeTestFilling = 0;
-        double timeTestGettingByIndex = 0;
-        double timeTestContains = 0;
-        for (int i = 0; i < COUNT; i++) {
-            timeTestFilling += ListTester.testFilling(new Vector<>());
-            timeTestGettingByIndex += ListTester.testGettingByIndex(new Vector<>());
-            timeTestContains += ListTester.testContains(new Vector<>());
-        }
-        System.out.println("Vector result:");
-        System.out.println("1) testFilling() = " + (timeTestFilling / COUNT) + " ms");
-        System.out.println("2) testGettingByIndex() = " + (timeTestGettingByIndex / COUNT) + " ms");
-        System.out.println("3) testContains() = " + (timeTestContains / COUNT) + " ms");
-        System.out.println();
+        return result;
     }
 }
